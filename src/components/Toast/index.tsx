328ec2f4c8653fc9c8ecd409b21a7f4ad4dcd85f
Mahvish <mahvish@Mahvishs-MacBook-Air.local>
2024-12-07 05:13:44 +0530

    removed unused code

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-type Props = {};
-
-export default function Toast({}: Props) {
+export default function Toast() {
   return (
     <div className="flex gap-5 items-center bg-[#222222] shadow-md w-[197px] h-[52px] rounded-[70px] text-white px-6 py-3 absolute bottom-10 right-[45%] slideUp">
       <div className="w-[28px] h-[28px] rounded-full bg-[#414141] flex justify-center items-center text-[14px]">
